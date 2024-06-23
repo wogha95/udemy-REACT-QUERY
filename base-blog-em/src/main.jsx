93c96c39359a60a3b4440aba434e487c68f3f3b9
wogha95 <wogha95@naver.com>
2024-06-23 14:35:08 +0900

    [base-blog-em] ReactQueryDevtools 설정

--- base-blog-em/src/main.jsx
+++ base-blog-em/src/main.jsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
+    <ReactQueryDevtools />
   </QueryClientProvider>
 );
