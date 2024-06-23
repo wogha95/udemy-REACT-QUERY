5f14f1302015ef085535330ba595841c424e3ddc
wogha95 <wogha95@naver.com>
2024-06-23 14:33:51 +0900

    [base-blog-em] isLoading, isError

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -9,13 +9,17 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
 
-  if (!data) {
-    return <div />;
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return <h3>{error}</h3>;
   }
 
   return (
