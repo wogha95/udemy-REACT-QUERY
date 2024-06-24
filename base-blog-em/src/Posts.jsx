d3e9298de6297def95d0f798d188b036b1c03718
wogha95 <wogha95@naver.com>
2024-06-24 22:15:49 +0900

    [base-blog-em] Pagination

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -6,12 +6,12 @@ import { fetchPosts } from "./api";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["posts", currentPage],
+    queryFn: ({ queryKey }) => fetchPosts(queryKey[1]),
   });
 
   if (isLoading) {
@@ -36,11 +36,17 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => setCurrentPage((prev) => prev + 1)}
+        >
           Next page
         </button>
       </div>
