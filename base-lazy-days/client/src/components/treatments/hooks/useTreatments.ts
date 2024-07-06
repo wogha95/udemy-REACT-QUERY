3279b74f488ebbd59da830cfa512452057622ea5
wogha95 <wogha95@naver.com>
2024-07-07 08:54:05 +0900

    [base-lazy-days] useIsFetching 적용

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -2,14 +2,18 @@ import type { Treatment } from "@shared/types";
 
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { useQuery } from "@tanstack/react-query";
 
-// for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get("/treatments");
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const fallback: Treatment[] = [];
+  const { data = fallback } = useQuery({
+    queryKey: [queryKeys.treatments],
+    queryFn: getTreatments,
+  });
+  return data;
 }
