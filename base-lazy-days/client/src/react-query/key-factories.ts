c789361ad89f4b20528cb4efcd1e438c55e764dc
wogha95 <wogha95@naver.com>
2024-07-09 22:17:24 +0900

    [base-lazy-days] generateUserKey erase userToken in dependency array

--- base-lazy-days/client/src/react-query/key-factories.ts
+++ base-lazy-days/client/src/react-query/key-factories.ts
@@ -1,7 +1,9 @@
 import { queryKeys } from "./constants";
 
 export function generateUserKey(userId: number, userToken: string) {
-  return [queryKeys.user, userId, userToken];
+  // deliberately exclude the userToken from the dependency array
+  //  to keep key consistent for userId regardless of token changes
+  return [queryKeys.user, userId];
 }
 
 export function generateUserAppointmentsKey(userId: number, userToken: string) {
