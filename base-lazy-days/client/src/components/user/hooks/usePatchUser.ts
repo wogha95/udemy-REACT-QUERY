a1ccd45afa8c5cfb717ccfdb1bbd53c7dc7bda6a
wogha95 <wogha95@naver.com>
2024-07-09 22:05:23 +0900

    [base-lazy-days] usePatchUser (setQueryData)

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,3 +1,5 @@
+import { useToast } from "@chakra-ui/react";
+import { useMutation } from "@tanstack/react-query";
 import jsonpatch from "fast-json-patch";
 
 import type { User } from "@shared/types";
@@ -6,32 +8,37 @@ import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 import { useUser } from "./useUser";
 
 // for when we need a server function
-// async function patchUserOnServer(
-//   newData: User | null,
-//   originalData: User | null,
-//  ): Promise<User | null> {
-//   if (!newData || !originalData) return null;
-//   // create a patch for the difference between newData and originalData
-//   const patch = jsonpatch.compare(originalData, newData);
+async function patchUserOnServer(
+  newData: User | null,
+  originalData: User | null
+): Promise<User | null> {
+  if (!newData || !originalData) return null;
+  // create a patch for the difference between newData and originalData
+  const patch = jsonpatch.compare(originalData, newData);
 
-//   // send patched data to the server
-//   const { data } = await axiosInstance.patch(
-//     `/user/${originalData.id}`,
-//     { patch },
-//     {
-//       headers: getJWTHeader(originalData.token),
-//     },
-//   );
-//   return data.user;
-//  }
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
+    `/user/${originalData.id}`,
+    { patch },
+    {
+      headers: getJWTHeader(originalData.token),
+    }
+  );
+  return data.user;
+}
 
 export function usePatchUser() {
   const { user, updateUser } = useUser();
 
-  // TODO: replace with mutate function
-  const patchUser = (newData: User | null) => {
-    // nothing to see here
-  };
+  const toast = useToast();
+
+  const { mutate: patchUser } = useMutation({
+    mutationFn: (newUser: User) => patchUserOnServer(newUser, user),
+    onSuccess: (userData: User | null) => {
+      toast({ title: "user updated!", status: "success" });
+      updateUser(userData);
+    },
+  });
 
   return patchUser;
 }
