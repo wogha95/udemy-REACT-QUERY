f96b162bd4104a7a04589e07c4ca5124995b0c26
wogha95 <wogha95@naver.com>
2024-07-15 22:21:48 +0900

    [base-lazy-days] Custom Hook testing

--- base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -1,10 +1,40 @@
-// import { act, renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 
 import { useAppointments } from "../hooks/useAppointments";
 import { AppointmentDateMap } from "../types";
 
-// import { createQueryClientWrapper } from "@/test-utils";
+import { createQueryClientWrapper } from "@/test-utils";
+
+function getAppointmentsCount(appointments: AppointmentDateMap) {
+  return Object.values(appointments).reduce(
+    (runningCount, appointmentsOnDate) =>
+      runningCount + appointmentsOnDate.length,
+    0
+  );
+}
 
 test("filter appointments by availability", async () => {
-  // test goes here
+  const { result } = renderHook(() => useAppointments(), {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for appointments to populate
+  await waitFor(() =>
+    expect(getAppointmentsCount(result.current.appointments)).toBeGreaterThan(0)
+  );
+
+  // appointments start out filtered (show only available)
+  const filteredAppointmentsLength = getAppointmentsCount(
+    result.current.appointments
+  );
+
+  // set to return all appointments
+  act(() => result.current.setShowAll(true));
+
+  // wait for count of appointments to be greater than when filtered
+  await waitFor(() =>
+    expect(getAppointmentsCount(result.current.appointments)).toBeGreaterThan(
+      filteredAppointmentsLength
+    )
+  );
 });
--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,28 +1,28 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render as RtlRender } from "@testing-library/react";
-import { ReactElement } from "react";
+import { PropsWithChildren, ReactElement } from "react";
 import { MemoryRouter } from "react-router-dom";
 
 import { queryClientOptions } from "@/react-query/queryClient";
 
 // ** FOR TESTING CUSTOM HOOKS ** //
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = () => {
-//   const queryClient = generateQueryClient();
-//   return ({ children }: PropsWithChildren) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+export const createQueryClientWrapper = () => {
+  const queryClient = generateQueryClient();
+  return ({ children }: PropsWithChildren) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
 
-function getnerateQueryClient() {
+function generateQueryClient() {
   queryClientOptions.defaultOptions.queries.retry = false;
   return new QueryClient(queryClientOptions);
 }
 
 // reference: https://testing-library.com/docs/react-testing-library/setup#custom-render
 function customRender(ui: ReactElement, client?: QueryClient) {
-  const queryClient = client ?? getnerateQueryClient();
+  const queryClient = client ?? generateQueryClient();
 
   return RtlRender(
     <ChakraProvider>
