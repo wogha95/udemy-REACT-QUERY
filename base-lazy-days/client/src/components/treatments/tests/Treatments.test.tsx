642111604503a27c59a8b7351a18de31eb82d2f2
wogha95 <wogha95@naver.com>
2024-07-15 21:25:31 +0900

    [base-lazy-days] Treatments.test.tsx

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,13 @@
 import { Treatments } from "../Treatments";
 
-import { render } from "@/test-utils";
+import { render, screen } from "@/test-utils";
 
-test("renders response from query", () => {
+test("renders response from query", async () => {
   render(<Treatments />);
+
+  const treatmentsTitles = await screen.findAllByRole("heading", {
+    name: /massage|facial|scrub/i,
+  });
+
+  expect(treatmentsTitles).toHaveLength(3);
 });
