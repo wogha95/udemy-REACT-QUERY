526ad217a634aac73952dda07759da7cd7d466c8
wogha95 <wogha95@naver.com>
2024-07-15 21:30:29 +0900

    [base-lazy-days] AllStaff.test.tsx (code quiz)

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,7 +1,13 @@
 import { AllStaff } from "../AllStaff";
 
-// import { render, screen } from "@/test-utils";
+import { render, screen } from "@/test-utils";
 
-test("renders response from query", () => {
-  // write test here
+test("renders response from query", async () => {
+  render(<AllStaff />);
+
+  const staffNames = await screen.findAllByRole("heading", {
+    name: /sandra|divya|mateo|michael/i,
+  });
+
+  expect(staffNames).toHaveLength(4);
 });
